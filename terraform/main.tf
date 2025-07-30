1ed1b6b13e2b6d63c4a39e4857496628546fd165
niteeshkumarvissakoti <klu2100032045@gmail.com>
2025-07-30 18:18:08 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = "ami-020cba7c55df1f615"
+  instance_type               = "t3.micro"
+  key_name                    = "test111"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "terra_ansi_pro"
+  }
+
+(*   provisioner "local-exec" {
+    command     = "echo ${self.public_ip} > ip.txt"
+    working_dir = "${path.module}"
+  }
+}
+ *)
+output "ec2_instance_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
